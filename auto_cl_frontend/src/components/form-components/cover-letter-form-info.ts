b03982bd0bbbd95ac97c13fad7b4fb8ad737317a
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-08-31 17:26:04 -0500

    form-informatin completed

--- auto_cl_frontend/src/components/form-components/cover-letter-form-info.ts
+++ auto_cl_frontend/src/components/form-components/cover-letter-form-info.ts
@@ -7,22 +7,75 @@ export const getCoverLetterFormInfo = (
   const returnInfo: ICoverLetterFormInfo = {
     Candidate: [
       {
-        question: "Candidate First Name: ",
+        question: "Candidate First Name",
         placeholder: "First Name",
         info: info.firstName,
         type: "UPDATE_FIRST_NAME",
         field: "string",
       },
       {
-        question: "Candidate Last Name: ",
+        question: "Candidate Last Name",
         placeholder: "Last Name",
         info: info.lastName,
         type: "UPDATE_LAST_NAME",
         field: "string",
       },
+      {
+        question: "Highlighted Skills",
+        placeholder: "React, Typescript, Node",
+        info: info.cover_letter.candidate_info.skills,
+        type: "UPDATE_SKILLS",
+        field: "stringArray",
+      },
+      {
+        question: "Highlighted Achievements",
+        placeholder:
+          "Lead dev team to decrease downtime by 20%, Leveraged AI tools to automate deployment tasks",
+        info: info.cover_letter.candidate_info.accomplishments,
+        type: "UPDATE_ACCOMPLISHMENTS",
+        field: "stringArray",
+      },
     ],
-    Company: [],
-    "Cover Letter": [],
+    Company: [
+      {
+        question: "Company Name",
+        placeholder: "SpaceX",
+        info: info.cover_letter.company_info.company_name,
+        type: "UPDATE_COMPANY_NAME",
+        field: "string",
+      },
+      {
+        question: "Position Title",
+        placeholder: "Software Engineer",
+        info: info.cover_letter.position_title,
+        type: "UPDATE_POSITION_TITLE",
+        field: "string",
+      },
+      {
+        question: "Related Company Values",
+        placeholder: "Hard Work, Integrity, Clean Code",
+        info: info.cover_letter.company_info.company_values,
+        type: "UPDATE_COMPANY_VALUES",
+        field: "stringArray",
+      },
+      {
+        question: "Personal Connection To Company",
+        placeholder:
+          "I really admire the company's dedication to the enviroment.",
+        info: info.cover_letter.company_info.company_connection,
+        type: "UPDATE_COMPANY_CONNECTION",
+        field: "string",
+      },
+    ],
+    "Cover Letter": [
+      {
+        question: "Paragraph Count",
+        placeholder: "3",
+        info: info.cover_letter.paragraph_count,
+        type: "UPDATE_COVER_LETTER_PARAGRAPH_COUNT",
+        field: "number",
+      },
+    ],
   };
 
   return returnInfo;
