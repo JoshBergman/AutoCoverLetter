0850c87774152581f7a7f5e0d5ad146359d40c68
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2023-09-10 10:27:08 -0500

    Analytics tracking

--- auto_cl_frontend/src/App.tsx
+++ auto_cl_frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
 
 import CoverLetterCurator from "./pages/cover-letter-curator";
+import { addSiteView } from "./analytics/view-call";
 
 const App = () => {
+  useEffect(() => {
+    addSiteView();
+  }, []);
   return (
     <div className="App">
       <header className="header">Auto Cover Letter</header>
